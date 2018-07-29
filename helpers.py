c2de58cff17858842c8a49c063303b61447a9cdb
Ridwan S <rms2176@users.noreply.github.com>
2018-07-30 07:00:21 +0900

    Initial commit
    What works:
    - Building a repository with dependencies
    
    What isn't implemented yet:
    - Iterative builds
    - Exporting per-package environment variables
    - Creating a dot file to enter the build environment
    - Downloading 3rd tarballs
    - Publishing the build artifacts

---
+++ helpers.py
@@ -0,0 +1,70 @@
+import errno
+import hashlib
+import os
+import stat
+
+from typing import Dict, List, Tuple
+
+import _hashlib  # This is only for typing, since hashlib functions return _hashlib.HASH objects
+
+
+class BuildInformation:
+    def __init__(self):
+        self.prefix = None
+        self.metadata_prefix = None
+
+
+def make_files_non_writeable(prefix: str) -> None:
+    for subdir, _dirs, files in os.walk(prefix):
+        for file in files:
+            filename = os.path.join(subdir, file)
+            new_mode = os.stat(filename).st_mode & ~stat.S_IWUSR & ~stat.S_IWGRP & ~stat.S_IWOTH
+            os.chmod(filename, new_mode)
+
+
+def hash_file(hash_object: _hashlib.HASH, file_name: str) -> None:
+    # This adds the content of the file to the provided _hashlib.HASH object.
+    # It does not return a hash of the file contents.
+    with open(file_name, 'rb') as file:
+        while True:
+            data = file.read(4096 * 4)
+            if not data:
+                break
+            hash_object.update(data)
+
+
+def get_file_hash(file_name: str) -> str:
+    sha1 = hashlib.sha1()
+    hash_file(sha1, file_name)
+    return sha1.hexdigest()
+
+
+def get_output_hashes_and_modes(directory) -> Dict[str, Tuple[str, int]]:
+    # TODO: This doesn't handle soft links
+    output_hashes_and_modes = {}
+    for subdir, _dirs, files in os.walk(directory):
+        for file in files:
+            file_name = os.path.join(subdir, file)
+            output_hashes_and_modes[file_name] = [get_file_hash(file_name), os.stat(file_name).st_mode]
+    return output_hashes_and_modes
+
+
+def get_builder_env(prefix: str):
+    return {
+        **os.environ,
+        "PREFIX": prefix,
+        "PATH": os.pathsep.join([os.path.join(prefix, "bin"),
+                                 os.environ["PATH"]]),
+    }
+
+
+def find_code_base_root() -> Tuple[str, str]:
+    # Return monorepository_root, code_base_name
+    current_dir = os.getcwd()
+
+    while current_dir != "/":
+        one_directory_up = os.path.dirname(current_dir)
+        if os.path.basename(one_directory_up) == "monorepository":
+            return one_directory_up, os.path.basename(current_dir)
+        current_dir = os.path.dirname(one_directory_up)
+    raise Exception("Could not find monorepository root.")
---
+++ monorepo.py
@@ -0,0 +1,184 @@
+# We disable this because fstrings are more readable than the older string formatting
+# and we don't care about the performance penalty.
+# pylint: disable=logging-fstring-interpolation
+
+import errno
+import hashlib
+import json
+import logging
+import os
+import shutil
+import subprocess
+import tempfile
+from typing import List, Tuple
+
+import argh
+import yaml
+
+import helpers
+
+
+MONOREPOSITORY_ROOT, CURRENT_CODE_BASE_NAME = helpers.find_code_base_root()
+CODEBASES = {}
+BUILD_INFORMATION = helpers.BuildInformation()
+
+
+def get_codebase(code_base_name):
+    if code_base_name not in CODEBASES:
+        CODEBASES[code_base_name] = CodeBase(code_base_name)
+
+    return CODEBASES[code_base_name]
+
+
+class CodeBase:
+    def __init__(self, code_base_name):
+        self.code_base_name = code_base_name
+        self.code_base_root = os.path.join(MONOREPOSITORY_ROOT, code_base_name)
+        logging.debug(f"Loading metadata for {code_base_name}...")
+        self._load_metadata()
+        self._compute_hash()
+        self.artifacts_json_file_name = os.path.join(BUILD_INFORMATION.metadata_prefix,
+                                                     f"artifacts-{self.code_base_name}-{self.hash.hexdigest()}.json")
+        logging.debug(f"Hash for {code_base_name} is {self.hash.hexdigest()}.")
+
+    def _load_metadata(self) -> None:
+        with open(os.path.join(self.code_base_root, "metadata.yaml")) as metadata_yaml:
+            self.metadata = yaml.load(metadata_yaml)
+
+    def _get_sorted_file_list(self) -> List[str]:
+        output = []
+        for subdir, _dirs, files in os.walk(self.code_base_root):
+            for file in files:
+                file_name = os.path.join(subdir, file)
+                output.append(file_name)
+        return sorted(output)
+
+    def _compute_hash(self) -> None:
+        self.hash = hashlib.sha1()
+        for dependency in self.metadata.get("dependencies", []):
+            depedency_code_base = get_codebase(dependency)
+            self.hash.update(depedency_code_base.hash.digest())
+        for file_name in self._get_sorted_file_list():
+            self.hash.update(file_name.encode("utf-8"))
+            self.hash.update(str(os.stat(file_name).st_mode).encode("utf-8"))
+            helpers.hash_file(self.hash, file_name)
+
+    def attempt_restore_previous_build(self) -> bool:
+        # Return if the restore succeeded.
+        # TODO: This can be further optimized by keeping a record of which codebases have been installed in the prefix.
+        # But in order to do this, we need to ensure code bases don't overwrite other code base's output.
+        logging.debug(f"Checking if {self.artifacts_json_file_name} exists...")
+        if os.path.isfile(self.artifacts_json_file_name):
+            with open(self.artifacts_json_file_name) as artifacts_json:
+                artifacts = json.load(artifacts_json)
+
+            self.output_hashes_and_modes = artifacts["files"]
+            cas_path = os.path.join(BUILD_INFORMATION.metadata_prefix, "cas")
+            for file_name, hash_and_mode in self.output_hashes_and_modes.items():
+                file_hash, file_mode = hash_and_mode
+                os.makedirs(os.path.dirname(file_name), exist_ok=True)
+                try:
+                    os.link(os.path.join(cas_path, file_hash), file_name)
+                except OSError as ose:
+                    if ose.errno != errno.EEXIST:
+                        raise
+                os.chmod(file_name, file_mode)
+            logging.debug(f"Restored {self.code_base_name} from previous build.")
+            return True
+        else:
+            logging.debug(f"It appears {self.code_base_name} was not previously built.")
+            return False
+
+    def build(self) -> None:
+        if self.attempt_restore_previous_build():
+            return
+
+        for dependency in self.metadata.get("dependencies", []):
+            depedency_code_base = get_codebase(dependency)
+            depedency_code_base.build()
+
+        logging.debug(f"Building {self.code_base_name}...")
+        stdout_log = open(os.path.join(BUILD_INFORMATION.metadata_prefix, f"{self.code_base_name}.out"), 'w')
+        stderr_log = open(os.path.join(BUILD_INFORMATION.metadata_prefix, f"{self.code_base_name}.err"), 'w')
+
+        # In order to avoid modifying the source (which would in turn modify the hash),
+        # we build in a temp dir.
+        with tempfile.TemporaryDirectory() as temp_dir_parent:
+            temp_dir = os.path.join(temp_dir_parent, self.code_base_name)
+            shutil.copytree(self.code_base_root, temp_dir)
+            os.chdir(temp_dir)
+            if os.path.isfile("build"):
+                command = ["./build"]
+            elif os.path.isfile("Makefile"):
+                command = ["make"]
+
+            try:
+                subprocess.run(command, check=True,
+                               env=helpers.get_builder_env(BUILD_INFORMATION.prefix),
+                               stdout=stdout_log, stderr=stderr_log)
+            finally:
+                stdout_log.close()
+                stderr_log.close()
+
+        helpers.make_files_non_writeable(BUILD_INFORMATION.prefix)
+        logging.debug(f"Built {self.code_base_name}.")
+        self.output_hashes_and_modes = helpers.get_output_hashes_and_modes(BUILD_INFORMATION.prefix)
+        logging.debug("Finished calculating hashes.")
+        self._record_build()
+        self._populate_cas()
+
+    def _record_build(self) -> None:
+        with open(self.artifacts_json_file_name, 'w') as artifacts_json:
+            json.dump({
+                "code_base": self.code_base_name,
+                "hash": self.hash.hexdigest(),
+                "files": self.output_hashes_and_modes,
+            }, artifacts_json)
+
+    def _populate_cas(self) -> None:
+        cas_path = os.path.join(BUILD_INFORMATION.metadata_prefix, "cas")
+        logging.debug("Populating content-addressable storage...")
+        os.makedirs(cas_path, exist_ok=True)
+        existing_blobs = set(os.listdir(cas_path))
+        for file_name, hash_and_mode in self.output_hashes_and_modes.items():
+            file_hash = hash_and_mode[0]
+            if file_hash in existing_blobs:
+                continue
+            os.link(file_name, os.path.join(cas_path, file_hash))
+            existing_blobs.add(file_hash)
+        logging.debug("Populated content-addresable storage.")
+
+
+@argh.arg("--prefix", help="Prefix to output build artifacts to")
+@argh.arg("--metadata-prefix", help="Prefix to output build metadata to; the metadata should be on the same file system as the prefix")
+@argh.arg("--debug", action="store_true", help="Enable debug logging")
+def build(prefix: str = None, metadata_prefix: str = None, debug: bool = False) -> None:
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig()
+
+    logging.debug(f"Mono-repository root is {MONOREPOSITORY_ROOT}.")
+    logging.debug(f"Current code base name is {CURRENT_CODE_BASE_NAME}.")
+    code_base_root = os.path.join(MONOREPOSITORY_ROOT, CURRENT_CODE_BASE_NAME)
+    os.chdir(code_base_root)
+
+    if prefix is None:
+        BUILD_INFORMATION.prefix = os.path.join(MONOREPOSITORY_ROOT, "prefix")
+    else:
+        BUILD_INFORMATION.prefix = prefix
+
+    if metadata_prefix is None:
+        BUILD_INFORMATION.metadata_prefix = os.path.join(MONOREPOSITORY_ROOT, "metadata_prefix")
+    else:
+        BUILD_INFORMATION.metadata_prefix = metadata_prefix
+
+    os.makedirs(BUILD_INFORMATION.prefix, exist_ok=True)
+    os.makedirs(BUILD_INFORMATION.metadata_prefix, exist_ok=True)
+
+    code_base = get_codebase(CURRENT_CODE_BASE_NAME)
+    code_base.build()
+
+
+if __name__ == "__main__":
+    argh.dispatch_commands([build])
