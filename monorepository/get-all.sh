438461e39f213ec5b160606a60189c58e3d0ff4e
Ridwan S <rms2176@users.noreply.github.com>
2018-09-18 21:27:58 +0900

    Add OpenSSL (Elixir requires it)

--- monorepository/get-all.sh
+++ monorepository/get-all.sh
@@ -10,3 +10,5 @@ wget --no-clobber https://archive.apache.org/dist/kafka/1.0.1/kafka_2.11-1.0.1.t
 wget --no-clobber http://erlang.org/download/otp_src_21.0.tar.gz
 wget --no-clobber https://github.com/elixir-lang/elixir/releases/download/v1.6.0/Precompiled.zip --output-document elixir-1.6.0-Precompiled.zip
 wget --no-clobber https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-6.3.2-linux-x86_64.tar.gz
+wget --no-clobber https://github.com/elixir-lang/elixir/archive/v1.6.0.tar.gz --output-document elixir-1.6.0.tar.gz
+wget --no-clobber https://www.openssl.org/source/openssl-1.1.1.tar.gz
